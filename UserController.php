0fde780eb8e96b7c3d84a80f8fb2581beb49921e
Dali8392 <mohamedali.mhadbi@esprit.tn>
2024-02-16 05:15:07 +0100

    gest-user-v2

--- UserController.php
+++ UserController.php
@@ -7,6 +7,8 @@ use App\Form\UserFormeType;
 use DateTime;
 use DateTimeZone;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    ////////thez li inscription wa fi nafes wa9et ta3mel add fel base wa tab3eth mail welcom
     #[Route('/inscription', name: 'app_inscription')]
     public function addUser(Request $request,EntityManagerInterface $entityManager): Response
     {  $user= new User(); 
@@ -45,6 +48,7 @@ class UserController extends AbstractController
             'form' => $form->createView(),
         ]);}
     }
+    /////////////valide form wa ken valid tab3eth mail fih code validation
     #[Route('/validate/user/form', name: 'validate_user_form')]
     public function validateUserForm(Request $request,EntityManagerInterface $entityManager)
     {  
@@ -79,6 +83,142 @@ class UserController extends AbstractController
         return new JsonResponse(['success' => false, 'form' => $htmlForm]);
    
     }
+    ///////////t'hez lil back-offic user
+    #[Route('/userAdmin', name: 'fetch_UserAdmin')]
+    public function fetch(Request $request): Response
+    {
+        $result = $this->getDoctrine()->getRepository(User::class)->findAll();
+    
+        return $this->render('user/backoffice.html.twig', [
+            'list' => $result,
+        ]);
+    }
+    /////////tfase5 user
+    #[Route('/user/delete/{id}', name: 'delet_user')]
+      public function delete($id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return new Response('User deleted successfully', Response::HTTP_OK);
+    }
+    ////////tbadel role li admine 
+    #[Route('/user/addAdmin/{id}', name: 'add_admin')]
+    public function addAdminAction($id): Response
+    {
+       
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        
+        $user->setRole('admin');
+
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        return new Response('Admin role added successfully');
+    }
+     
+    //////tbadel role li user
+    #[Route('/user/deleteAdmin/{id}', name: 'delete_admin')]
+    public function deleteAdminAction($id): Response
+    {
+       
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+       
+        $user->setRole('user');
+
+       
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        return new Response('Admin role deleted successfully');
+    }
+     ////////modif user 
+    #[Route('/user/modif/{id}', name: 'modif_user')]
+    public function modif($id , Request $request,ManagerRegistry $managerRegistry): Response
+    {
+        
+        $user = $managerRegistry->getManager()->getRepository(User::class)->findOneBy(['id' => $id]);
+        $form = $this->createForm(UserFormeType::class, $user);
+
+       $form->handleRequest($request);
+       
+         if ($form->isSubmitted() && $form->isValid()) {
+           $managerRegistry->getManager()->persist($user);
+            $managerRegistry->getManager()->flush();
+             return $this->redirectToRoute('fetch_UserAdmin');
+        }
+        return $this->render('user/editProfile.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+     ////////reset password
+
+ #[Route('/user/resetPassword', name: 'reset_password')]
+    public function resetPassord(Request $request,ManagerRegistry $managerRegistry): JsonResponse
+    {
+        $id = $request->get('userId');
+        $user = $managerRegistry->getManager()->getRepository(User::class)->findOneBy(['id' => $id]);
+        $randomCode = $this->generateRandomCode();
+        $templatePath = $this->getParameter('kernel.project_dir') . '/templates/emails/alertMail.html.twig';
+        $message = file_get_contents($templatePath);
+        $message=str_replace("20User20",$user->getName(),$message);
+        $message=str_replace("20Code20",$randomCode,$message);
+
+        $success=$this->sendMail($message,'Password Reset Alert',$user);
+              return new JsonResponse(['success' => $success ,'code'=> $randomCode]);
+       
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /////////////////////////////////////
     
     private function generateRandomCode()
     {
@@ -93,14 +233,16 @@ class UserController extends AbstractController
 
         return $code;
     }
-    private function sendMail($message,$subject,User $user)
+    private function sendMail($message,$subject,User $user):bool
     {
         require_once __DIR__ . '/../../public/mail.php';
         $mail->setFrom('mhama6970@gmail.com', 'HawesBiya@noReplay');
         $mail->addAddress($user->getEmail());
         $mail->Subject = $subject;
         $mail->Body    = $message;
-        $mail->send();
+        
+            return $mail->send();
+       
     }
     private function generateId(): string
   {
